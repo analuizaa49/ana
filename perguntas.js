ac0869f37ea7925d05de46b299fffc003347addc
analuizaa49 <ana.rodrigues.gomes@escola.pr.gov.br>
2024-10-04 15:19:57 -0300

    fff

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,10 @@ criaCartao(
 'o que a natureza transmite?',
 'Além dos beneficios fisicosa, conexão com natureza tambem traz vantagens para saúde mental: redução de ansiedade e depressão'
 )
+
+criarCartao(
+'geografia',
+'por que plutao deixou de ser um planeta?',
+' plutao é aproximadamente 5,9 bilhoes de quilometros do sol, e por conta dele ser -232°C plutao deixou de ser um planeta habitavel'
+
+)
