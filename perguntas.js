bc381b6c2d1106198060717b0ada625d18ff6ee4
analuizaa49 <ana.rodrigues.gomes@escola.pr.gov.br>
2024-10-04 15:08:45 -0300

    sdsf

---
+++ perguntas.js
@@ -0,0 +1,5 @@
+criaCartao(
+'natureza',
+'o que a natureza transmite?',
+'Além dos beneficios fisicosa, conexão com natureza tambem traz vantagens para saúde mental: redução de ansiedade e depressão'
+)
